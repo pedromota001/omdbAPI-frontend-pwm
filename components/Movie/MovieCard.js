2a79add5044df45cde3162e93b78df7a99cb68cc
Guga2111 <luisgosampaio@gmail.com>
2025-04-08 18:46:08 -0300

    feat: imagem para erro implementada, tratamento de exceção para imagem e sua implementaçao feita, funcao para retorna uma string que atraves de interpolação defina a cor da cada filme

--- components/Movie/MovieCard.js
+++ components/Movie/MovieCard.js
@@ -1,9 +1,21 @@
 import React from "react";
-//import de styles.css que vai ser implementada
+import "../styles.css";
 
 export default function MovieCard({ movie }) {
+  const handleError = (e) => {
+    e.target.src = "images/default.jpg";
+  };
+
+  //funcao para definir a cor do 'rating' baseado no css
+  const getRatingClass = (rating) => {
+    if (rating >= 8) return "rating-good";
+    if (rating >= 5 && rating < 8) return "rating-ok";
+    return "rating-bad";
+  };
+
   return (
     <div key={movie.id} className="movie-card">
+      <img src={movie.poster} alt={movie.title} onError={handleError}></img>
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <div>
