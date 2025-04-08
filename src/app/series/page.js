d666cd333dca0f56d3c91edadd0da6d4c43155dd
Guga2111 <luisgosampaio@gmail.com>
2025-04-08 19:15:13 -0300

    feat: pagina de series

--- src/app/series/page.js
+++ src/app/series/page.js
@@ -1,12 +1,46 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
+import MovieGrid from "../../../components/Movie/MovieGrid.jsx";
+import axios from "axios";
 
-export default function SeriesPage(){
-    return(
-        <>
-        <h1>pagina de series</h1>
-        </>
-    )
+export default function SeriesPage() {
+  const [series, setSeries] = useState([]);
+
+  async function carregar_series() {
+    try {
+      const response = await axios.get(
+        "https://parseapi.back4app.com/classes/Filme",
+        {
+          headers: {
+            "X-Parse-Application-Id":
+              "GwnUACA5KJuULzj5Pf30JZhwXU0lkeu43Z1wnDoN",
+            "X-Parse-REST-API-Key": "8wYzUlStyJkZFCgAh1aHHy035JPU1e8wNhgRtBqp",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return response.data.results;
+    } catch (err) {
+      alert("Erro ao carregar reviews", err);
+      return [];
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const series = await carregar_series();
+      setMovies(series);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>pagina de series</h1>
+      <MovieGrid series={series}></MovieGrid>
+    </>
+  );
 }
 
 /*a ser implementada*/
