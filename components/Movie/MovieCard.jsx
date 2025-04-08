fff15ab8e666986c49d1c3029ddd0c75fa24e825
Guga2111 <luisgosampaio@gmail.com>
2025-04-08 19:07:20 -0300

    feat: getRatingClass para retorna interpolaçao baseada na nota e assim definir um cor para essa nota

--- components/Movie/MovieCard.jsx
+++ components/Movie/MovieCard.jsx
@@ -5,15 +5,21 @@ export default function MovieCard({ movie }) {
   const handleError = (e) => {
     e.target.src = "images/default.jpg";
   };
+
+  const getRatingClass = (notaIMDB) => {
+    if (notaIMDB >= 8) return "rating-good";
+    if (notaIMDB >= 5 && notaIMDB < 8) return "rating-ok";
+    return "rating-bad";
+  };
   
   return (
     <div key={movie.id} className="movie-card">
-      <img src={movie.poster} alt={movie.title} onError={handleError}></img>
+      <img src={movie.posterUrl} alt={movie.title} onError={handleError}></img>
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.titulo}</h3>
         <div>
           <span className="movie-card-genre">{movie.genero}</span>
-          <span className={"movie-card-rating"}>
+          <span className={`movie-card-rating ${getRatingClass(movie.notaIMDB)}`}>
             {movie.notaIMDB}
           </span>
         </div>
