ee3826724536ea7064ca1e00092c46602af8c51b
Guga2111 <luisgosampaio@gmail.com>
2025-04-11 00:42:17 -0300

    feat: pesquisa feita

--- components/Movie/MovieGrid.jsx
+++ components/Movie/MovieGrid.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import MovieCard from "./MovieCard.jsx";
 import "./styles.css"
 
 export default function MovieGrid({ movies, fluxo }) {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.titulo.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      matchesSearchTerm(movie, searchTerm)
+  );
+
   return (
     <div>
       <input
         type="text"
         className="search-input"
         placeholder="Search Movies..."
+        value={searchTerm}
+        onChange={handleSearchTerm}
       />
       <div className="movies-grid">
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <MovieCard key={movie.objectId} movie={movie} fluxo={fluxo} />
         ))}
       </div>
